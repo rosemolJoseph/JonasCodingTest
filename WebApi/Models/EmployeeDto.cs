947f8ecf296f7c44eebca362fa8425c66ccb429f
rosemolJoseph <121903306+rosemolJoseph@users.noreply.github.com>
2023-01-04 00:00:30 -0600

    Update EmployeeDto.cs

--- WebApi/Models/EmployeeDto.cs
+++ WebApi/Models/EmployeeDto.cs
@@ -7,10 +7,10 @@ namespace WebApi.Models
     {
         public string EmployeeCode{ get; set; }
         public string EmployeeName { get; set; }
-        public string Occupation { get; set; }
+        public string OccupationName { get; set; }
         public string EmployeeStatus { get; set; }
         public string EmailAddress { get; set; }
         public string Phone { get; set; }
-        public DateTime LastModified { get; set; }
+        public DateTime LastModifiedDateTime { get; set; }
     }
 }
