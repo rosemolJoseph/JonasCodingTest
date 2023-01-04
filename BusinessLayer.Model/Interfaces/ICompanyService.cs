caf06d250cbbfb787aa82cf40f322dd2ccf2d492
rosemolJoseph <121903306+rosemolJoseph@users.noreply.github.com>
2023-01-03 22:40:34 -0600

    1 and 2

--- BusinessLayer.Model/Interfaces/ICompanyService.cs
+++ BusinessLayer.Model/Interfaces/ICompanyService.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using BusinessLayer.Model.Models;
 
 namespace BusinessLayer.Model.Interfaces
@@ -7,5 +7,8 @@ namespace BusinessLayer.Model.Interfaces
     {
         IEnumerable<CompanyInfo> GetAllCompanies();
         CompanyInfo GetCompanyByCode(string companyCode);
+        bool SaveCompanyDetails(CompanyInfo companyInfo);
+        bool UpdateCompanyDetails(string companyCode, CompanyInfo companyInfo);
+        bool DeleteCompanyDetails(string companyCode);
     }
 }
