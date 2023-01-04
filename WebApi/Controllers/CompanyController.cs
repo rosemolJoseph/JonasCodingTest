569e8f5fc7a173dba9dcea6640721a319f7b71c3
rosemolJoseph <121903306+rosemolJoseph@users.noreply.github.com>
2023-01-04 00:20:32 -0600

    Update CompanyController.cs

--- WebApi/Controllers/CompanyController.cs
+++ WebApi/Controllers/CompanyController.cs
@@ -11,45 +11,77 @@ namespace WebApi.Controllers
     {
         private readonly ICompanyService _companyService;
         private readonly IMapper _mapper;
+         private readonly ILogger _logger;
 
-        public CompanyController(ICompanyService companyService, IMapper mapper)
+        public CompanyController(ICompanyService companyService, IMapper mapper,ILogger logger)
         {
             _companyService = companyService;
             _mapper = mapper;
+            _logger = logger;
         }
         // GET api/<controller>
         public async IEnumerable<CompanyDto> GetAll()
         {
+        try {
             var items = await _companyService.GetAllCompanies();
             return _mapper.Map<IEnumerable<CompanyDto>>(items);
+            }
+            catch(Exception e)
+            {
+            _logger.log(e);
+            }
         }
 
         // GET api/<controller>/5
         public async CompanyDto Get(string companyCode)
         {
+        try{
             var item = await _companyService.GetCompanyByCode(companyCode);
             return _mapper.Map<CompanyDto>(item);
+            }
+             catch(Exception e)
+            {
+            _logger.log(e);
+            }
         }
 
         // POST api/<controller>
         public async bool Post(CompanyDto companyDto)
         {
+        try{
             var result = await _companyService.SaveCompanyDetails(_mapper.Map<CompanyInfo>(companyDto));
             return result;
+            }
+             catch(Exception e)
+            {
+            _logger.log(e);
+            }
         }
 
         // PUT api/<controller>/5
         public async bool Put(string companyCode, CompanyDto companyDto)
         {
+        try{
             var result = await _companyService.SaveCompanyDetails(string companyCode, _mapper.Map<CompanyInfo>(companyDto));
             return result;
+            }
+             catch(Exception e)
+            {
+            _logger.log(e);
+            }
         }
 
         // DELETE api/<controller>/5
         public async bool Delete(string companyCode)
         {
+        try{
             var result = await _companyService.DeleteCompanyDetails(string companyCode);
             return result;
+            }
+             catch(Exception e)
+            {
+            _logger.log(e);
+            }
         }
     }
 }
