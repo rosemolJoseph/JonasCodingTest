8d72207abec0194a124489b90c46183d51c823a6
rosemolJoseph <121903306+rosemolJoseph@users.noreply.github.com>
2023-01-04 00:02:12 -0600

    Update EmployeeInfo.cs

--- BusinessLayer.Model/Models/EmployeeInfo.cs
+++ BusinessLayer.Model/Models/EmployeeInfo.cs
@@ -7,11 +7,12 @@ namespace BusinessLayer.Model.Models
     {
         public string EmployeeCode{ get; set; }
         public string EmployeeName { get; set; }
-        public string Occupation { get; set; }
+        public string CompanyName { get; set; }
+        public string OccupationName { get; set; }
         public string EmployeeStatus { get; set; }
         public string EmailAddress { get; set; }
         public string Phone { get; set; }
         public string Country { get; set; }
-        public DateTime LastModified { get; set; }
+        public DateTime LastModifiedDateTime { get; set; }
     }
 }
