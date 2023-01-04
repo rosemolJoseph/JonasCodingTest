2c92658f017ff3588ac288e48276b6e6f28584ad
rosemolJoseph <121903306+rosemolJoseph@users.noreply.github.com>
2023-01-03 23:15:09 -0600

    Update ICompanyService.cs

--- BusinessLayer.Model/Interfaces/ICompanyService.cs
+++ BusinessLayer.Model/Interfaces/ICompanyService.cs
@@ -7,5 +7,8 @@ namespace BusinessLayer.Model.Interfaces
     {
         IEnumerable<CompanyInfo> GetAllCompanies();
         CompanyInfo GetCompanyByCode(string companyCode);
+        bool SaveCompanyDetails(CompanyInfo companyInfo);
+        bool UpdateCompanyDetails(string companyCode, CompanyInfo companyInfo);
+        bool DeleteCompanyDetails(string companyCode);
     }
 }
